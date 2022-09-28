91cc779ca20615192e9e6a8701278d3ec34ded15
Dima <dmitrijskurcenko3@gmail.com>
2022-09-28 11:16:26 +0300

    Learn2
    Learn2

---
+++ src/separator/Separator.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package separator;
+
+/**
+ *
+ * @author admin
+ */
+public class Separator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      String c="Hellow";
+      String d="Word";
+      String g="";
+      if(c!=null){
+          System.out.println(c+""+d);
+        
+          
+      }else{
+          System.out.println("Try it again");
+      }
+      
+    }
+    
+}
